69d98e69ca8bf8c2ec3ff4eb6c920b6e718caa27
maruizero <marui.yuri.last.military.zero@gmail.com>
2023-06-01 17:53:56 +0900

    ヘーゲル哲学のファイルを置いた。

---
+++ add_commit_push.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+echo gitを使用します
+
+git add .
+
+echo addしました。
+
+echo commitします。エディターにコメントを入力してください。
+
+git commit
+
+echo commit完了しました。
+
+git push
+
+echo pushが完了しました。
+
+read -p "エンターキーを押して終了。"
